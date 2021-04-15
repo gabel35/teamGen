902563a60fb7ed4b83d425e4a36c36eedef6c013
gabel35 <gabrieljose3135@gmail.com>
2021-04-14 22:19:14 -0400

    made generating html function

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const Employee = require("./lib/Employee");
 
 
 const employees = [];
+let bodiesHTML = ""
 
 const askQuestions = async () => {
 
@@ -54,7 +55,7 @@ const askQuestions = async () => {
             type: "confirm",
             name: "another",
             message: "Would you like to add another employee?",
-            default: true
+            default: false
         }
     ]
     
@@ -77,14 +78,100 @@ const askQuestions = async () => {
                 console.log(employees);
                 break;
             default:
-                console.log("is anyone there?")
+                console.log("generating directory")
                 break;
         }
         return another ? askQuestions(employees) : employees
     };
 
+
+    function genDir(employees) {
+        var content = topHTML + bodiesHTML + bottomHTML;
+        var topHTML = `
+        <head>
+            <meta charset="UTF-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous" />
+            <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
+            <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+            <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-Piv4xVNRyMGpqkS2by6br4gNJ7DXjqk09RmUpJ8jgGtD7zP9yug3goQfGII0yAns" crossorigin="anonymous"></script>
+            <title>Team Profile Generator</title>
+        </head>
+        <body>
+        <div class="jumbotron jumbotron-fluid">
+            <div class="container">
+              <h1 class="display-4">Employee Directory</h1>
+            </div>
+        </div>
+        <div class="container">
+            <div class="row">
+        `
+        var bottomHTML = `
+                </div>
+            </div>
+        </body>
+        </html>
+        `
+        bodiesHTML = ""
+        employees.map(function (employee) {
+            if (employee.role === "manager") {
+                bodiesHTML += `
+                    <div class= "col-md-4">
+                        <div class="card">
+                            <div class="card-body">
+                                <h2 class="card-title"> Name: ${employee.name}</h2>
+                                <p class="card-text"> ${employee.role}</p>
+                            </div>
+                            <ul class="list-group list-group-flush">
+                                <li class="list-group-item">ID: ${employee.id}</li>
+                                <li class="list-group-item">Email:<a href="mailto:${employee.email}" target="_blank">${employee.email}</a></li>
+                                <li class="list-group-item">Office Number: ${employee.office} </li>
+                            </ul>
+                        </div>
+                    </div>
+                `;
+            } else if (employee.role === "intern") {
+                bodiesHTML += `
+                    <div class= "col-md-4">
+                        <div class="card" >
+                            <div class="card-body">
+                            <h2 class="card-title">Name:${employee.name}</h2>
+                                <p class="card-text"> ${employee.role} </p>
+                            </div>
+                            <ul class="list-group list-group-flush">
+                                <li class="list-group-item">ID: ${employee.id}</li>
+                                <li class="list-group-item">Email: <a href="${employee.email}" target="_blank">${employee.email}</a></li>
+                                <li class="list-group-item">School: ${employee.school}</li>
+                            </ul>
+                        </div>
+                    </div>
+                `;
+            } else if (employee.role === "engineer") {
+                bodiesHTML += `
+                    <div class= "col-md-4">
+                        <div class="card">
+                            <div class="card-body">
+                                <h2 class="card-title"> Name: ${employee.name}</h2>
+                                <p class="card-text"> ${employee.role} </p>
+                            </div>
+                            <ul class="list-group list-group-flush">
+                                <li class="list-group-item">ID:  ${employee.id}</li>
+                                <li class="list-group-item">Email: <a href="${employee.email}">${employee.email}</a></li>
+                                <li class="list-group-item">GitHub: <a href="https://github.com/${employee.github}" target="_blank">${employee.github}</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                `;
+            }
+        });
+        fs.writeFile("./dist/Directory.html", content, (err) => {
+            err ? console.log(err) : console.log("Content has been generated")
+        });
+    };
+
     const init = async () => {
         await askQuestions();
+        await genDir(employees);
     }
     
     init();
